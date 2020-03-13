20df29de10198236504687360dbaf87084fcde85
Edward Osorno <edward-1094@hotmail.com>
2020-03-13 14:34:29 +0000

    add

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -14,13 +14,13 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	size_t r = 0, l = 0;
 
 	if (!tree)
-		return 0;
+		return (0);
 	if (tree->left)
 		l = 1 + binary_tree_height(tree->left);
 	if (tree->right)
 		r = 1 + binary_tree_height(tree->right);
 	if (l >= r)
-		return l;
+		return (l);
 	else
-		return r;
+		return (r);
 }
