0ee0e3e821bb4633b9889ed04c95827c7452cb2f
Edward Osorno <edward-1094@hotmail.com>
2020-03-13 00:30:55 +0000

    add

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -17,7 +17,12 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 	node = malloc(sizeof(binary_tree_t));
 	if (!node)
 		return (NULL);
-	node->parent = parent;
+	if (parent)
+		node->parent = parent;
+	else
+		node->parent = NULL;
 	node->n = value;
+	node->left = NULL;
+	node->right = NULL;
 	return (node);
 }
