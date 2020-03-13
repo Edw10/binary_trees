c1bfd611395fb21611ca9b0de704a2440acd2e33
Edward Osorno <edward-1094@hotmail.com>
2020-03-13 18:13:38 +0000

    add

---
+++ 18-binary_tree_uncle.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include "binary_trees.h"
+
+/**
+ * binary_tree_uncle - return the node's uncle
+ * @node: a pointer to the node to evaluate
+ * Return: the node's depth
+ */
+
+binary_tree_t *binary_tree_uncle(binary_tree_t *node)
+{
+	if (!node || !(node->parent) || !(node->parent->parent))
+		return (NULL);
+	if (node->parent-parent->left && node->parent->parent->right)
+	{
+		if (node->parent-parent->left == node->parent)
+			return (node->parent->parent->right);
+		else
+			return (node->parent->parent->left);
+	}
+	return (NULL);
+}
