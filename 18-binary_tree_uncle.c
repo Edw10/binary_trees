f0b7dc434bf1fd8ebd8f0c0d4acb7a1c742878f2
Edward Osorno <edward-1094@hotmail.com>
2020-03-13 18:17:03 +0000

    add

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -13,9 +13,9 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
 	if (!node || !(node->parent) || !(node->parent->parent))
 		return (NULL);
-	if (node->parent-parent->left && node->parent->parent->right)
+	if (node->parent->parent->left && node->parent->parent->right)
 	{
-		if (node->parent-parent->left == node->parent)
+		if (node->parent->parent->left == node->parent)
 			return (node->parent->parent->right);
 		else
 			return (node->parent->parent->left);
